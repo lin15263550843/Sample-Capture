2ddb0fbf877b1f5ab82f7288e9a7864cae6a4feb
Steve Tsokev <stzokev@gmail.com>
2013-09-19 06:31:56 -0500

    fixed js error

--- sample-capture/scripts/main.js
+++ sample-capture/scripts/main.js
@@ -1,77 +1,78 @@
 document.addEventListener("deviceready", onDeviceReady, false);
  
 function id(element) {
-	return document.getElementById(element);
+    return document.getElementById(element);
 }
 
 function onDeviceReady() {
-	captureApp = new captureApp();
-	captureApp.run();
+    captureApp = new captureApp();
+    captureApp.run();
 }
 
 function captureApp() {
 }
 
 captureApp.prototype = {
-	pictureSource:null,
+    pictureSource:null,
     
-	destinationType:null,
+    destinationType:null,
     
-	run:function() {
-		var that = this;
-		id("captureVideo").addEventListener("click", function() {
-			that._captureVideo.apply(that, arguments);
-		});
-		id("captureAudio").addEventListener("click", function() {
-			that._capureAudio.apply(that, arguments);
-		});
-		id("captureImage").addEventListener("click", function() {
-			that._captureImage.apply(that, arguments);
-		});
-	},
+    run:function() {
+        var that = this;
+        id("captureVideo").addEventListener("click", function() {
+            that._captureVideo.apply(that, arguments);
+        });
+        id("captureAudio").addEventListener("click", function() {
+            that._capureAudio.apply(that, arguments);
+        });
+        id("captureImage").addEventListener("click", function() {
+            that._captureImage.apply(that, arguments);
+        });
+    },
     
-	_captureVideo:function() {
-		var that = this;
-		navigator.device.capture.captureVideo(function() {
-			that._captureSuccess.apply(that, arguments);
-		}, function() { 
-			captureApp._captureError.apply(that, arguments);
-		}, {limit:1});
-	},
+    _captureVideo:function() {
+        var that = this;
+        navigator.device.capture.captureVideo(function() {
+            that._captureSuccess.apply(that, arguments);
+        }, function() { 
+            captureApp._captureError.apply(that, arguments);
+        }, {limit:1});
+    },
     
-	_capureAudio:function() {
-		var that = this;
-		navigator.device.capture.captureAudio(function() {
-			that._captureSuccess.apply(that, arguments);
-		}, function() { 
-			captureApp._captureError.apply(that, arguments);
-		}, {limit:1});
-	},
+    _capureAudio:function() {
+        var that = this;
+        navigator.device.capture.captureAudio(function() {
+            that._captureSuccess.apply(that, arguments);
+        }, function() { 
+            captureApp._captureError.apply(that, arguments);
+        }, {limit:1});
+    },
     
-	_captureImage:function() {
-		var that = this;
-		navigator.device.capture.captureImage(function() {
-			that._captureSuccess.apply(that, arguments);
-		}, function() { 
-			captureApp._captureError.apply(that, arguments);
-		}, {limit:1});
-	},
+    _captureImage:function() {
+        var that = this;
+        navigator.device.capture.captureImage(function() {
+            that._captureSuccess.apply(that, arguments);
+        }, function() { 
+            captureApp._captureError.apply(that, arguments);
+        }, {limit:1});
+    },
     
-	_captureSuccess:function(capturedFiles) {
-		var i,
-		media = document.getElementById("media");
-		media.innerHTML = "";
-		for (i=0;i < capturedFiles.length;i+=1) {
-			media.innerHTML+='<p>Capture taken! Its path is: ' + capturedFiles[i].fullPath + '</p>'
-		}
-	},
+    _captureSuccess:function(capturedFiles) {
+        var i,
+        media = document.getElementById("media");
+        media.innerHTML = "";
+        for (i=0;i < capturedFiles.length;i+=1) {
+            media.innerHTML+='<p>Capture taken! Its path is: ' + capturedFiles[i].fullPath + '</p>'
+        }
+    },
     
-	_captureError:function(error) {
-		if (device.uuid == "e0101010d38bde8e6740011221af335301010333" || device.uuid == "e0908060g38bde8e6740011221af335301010333") {
-			alert(error);
-		}
-		else
-			var media = document.getElementById("media");
-		media.innerHTML = "An error occured! Code:" + error.code;
-	},
+    _captureError:function(error) {
+        if (device.uuid == "e0101010d38bde8e6740011221af335301010333" || device.uuid == "e0908060g38bde8e6740011221af335301010333") {
+            alert(error);
+        }
+        else {
+            var media = document.getElementById("media");
+            media.innerHTML = "An error occured! Code:" + error.code;
+        }
+    },
 }
